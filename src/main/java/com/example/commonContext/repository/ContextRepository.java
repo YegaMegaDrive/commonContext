07d25361a47e5389d327c569674005a9b72e44c4
grigolenok <egor.grigolenok@mail.ru>
2022-01-06 16:42:55 +0300

    check modules rabotosposobnost

--- src/main/java/com/example/commonContext/repository/ContextRepository.java
+++ src/main/java/com/example/commonContext/repository/ContextRepository.java
@@ -3,6 +3,7 @@ package com.example.commonContext.repository;
 import com.example.commonContext.entity.CommonContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
@@ -17,13 +18,14 @@ public class ContextRepository {
     private HashOperations<String, Object, Object> hashOperations;
     public static final String KEY = "Context";
 
+    @Autowired
     public ContextRepository(RedisTemplate<String,?> redisTemplate) {
         this.hashOperations = redisTemplate.opsForHash();
     }
 
     public void createContext(String token,CommonContext context) {
-        hashOperations.put(KEY, context.getToken(), context);
-        log.info(String.format("Context with TOKEN %s saved", context.getToken()));
+        hashOperations.put(KEY, token, context);
+        log.info(String.format("Context with TOKEN %s saved", token));
     }
 
     public CommonContext getContext(String token) {
