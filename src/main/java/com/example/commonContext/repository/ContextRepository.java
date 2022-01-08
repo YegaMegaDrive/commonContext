47fb7639aa846944b5bf7f9aa3797922d5373e13
grigolenok <egor.grigolenok@mail.ru>
2022-01-08 10:36:57 +0300

    check modules rabotosposobnost

--- src/main/java/com/example/commonContext/repository/ContextRepository.java
+++ src/main/java/com/example/commonContext/repository/ContextRepository.java
@@ -1,48 +1,13 @@
 package com.example.commonContext.repository;
 
 import com.example.commonContext.entity.CommonContext;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Repository;
 
-import java.util.Map;
+public interface ContextRepository {
+    void createContext(String token, CommonContext context);
 
-@Repository
-@RequiredArgsConstructor
-@Slf4j
-public class ContextRepository {
+   CommonContext getContext(String token);
 
-    private HashOperations<String, Object, Object> hashOperations;
-    public static final String KEY = "Context";
+   void updateContext(CommonContext context);
 
-    @Autowired
-    public ContextRepository(RedisTemplate<String,?> redisTemplate) {
-        this.hashOperations = redisTemplate.opsForHash();
-    }
-
-    public void createContext(String token,CommonContext context) {
-        hashOperations.put(KEY, token, context);
-        log.info(String.format("Context with TOKEN %s saved", token));
-    }
-
-    public CommonContext getContext(String token) {
-        return (CommonContext) hashOperations.get(KEY, token);
-    }
-
-   /* public Map<Object, Object> getAll(){
-        return hashOperations.entries(KEY);
-    }*/
-
-    public void updateContext(CommonContext context) {
-        hashOperations.put(KEY, context.getToken(), context);
-        log.info(String.format("Context with TOKEN %s updated", context.getToken()));
-    }
-
-    public void deleteContext(String token) {
-        hashOperations.delete(KEY, token);
-        log.info(String.format("Context with TOKEN %s deleted", token));
-    }
+   void deleteContext(String token);
 }
---
+++ src/main/java/com/example/commonContext/service/ContextService.java
@@ -0,0 +1,4 @@
+package com.example.commonContext.service;
+
+public interface ContextService {
+}
