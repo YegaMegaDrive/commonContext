6fdd475b14b889b4bb68ae323583c6bb96d0a547
grigolenok <egor.grigolenok@mail.ru>
2022-01-11 18:09:23 +0300

    add context service methods

--- src/main/java/com/example/commonContext/service/ContextService.java
+++ src/main/java/com/example/commonContext/service/ContextService.java
@@ -1,4 +1,18 @@
 package com.example.commonContext.service;
 
-public interface ContextService {
+import com.example.commonContext.entity.CommonContext;
+
+import java.time.Duration;
+
+public interface ContextService<N,V> {
+
+    void createContextField(String token, CommonContext context);
+
+    CommonContext getContextField(String token);
+
+    void updateContextField(N fieldName,V fieldValue, String token);
+
+    void deleteContextField(String token);
+
+    void refreshTimeToLive(String token);
 }
--- src/main/java/com/example/commonContext/service/ContextServiceImpl.java
+++ src/main/java/com/example/commonContext/service/ContextServiceImpl.java
@@ -4,12 +4,11 @@ import com.example.commonContext.repository.ContextRepository;
 import com.example.commonContext.repository.ContextRepositoryImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class ContextServiceImpl implements ContextService{
+public class ContextServiceImpl<N,V> implements ContextService<N,V>{
 
     private final ContextRepository repository;
 
@@ -17,6 +16,8 @@ public class ContextServiceImpl implements ContextService{
     public ContextServiceImpl(ContextRepositoryImpl contextRepository) {
         this.repository = contextRepository;
     }
+
+
 
 
 }
